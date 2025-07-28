28ccba5b51ddba387d4652df388680cbe84ca2cb
inderjit27 <iam.inderjitsingh1@gmail.com>
2025-07-29 00:16:27 +0530

    change Ui...

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,23 @@ const App = () => {
 
   const TL1 = gsap.timeline()
 
+  
+  const screenWidth = window.innerWidth;
+  let rightRotate = 20;
+  let leftRotate = -20;
+  
+  if (screenWidth < 768) {
+    rightRotate = 8;
+    leftRotate = -8;
+  } else if (screenWidth < 1024) {
+    rightRotate = 12;
+    leftRotate = -12;
+  }
+  
   // ---------------------------------------- FUNCTIONS
 
   const welcomeAnimation = () => {
+
     TL1.to('.point', {
       y: 0,
       duration: 1,
@@ -89,8 +103,51 @@ const App = () => {
       duration: 0.02,
       ease: 'power1.inOut'
     }, 'a2')
+
+    // Title For All Devices 
+    TL1.to('.TTtitle, .TT-p',{
+      opacity:1,
+      stagger:0.3,
+      duration:0.7,
+      ease: 'power3.inOut'
+
+    },'a3')
+   
+
+    // 3_IMG
+    TL1.to('.C2_img_main', {
+      delay: -0.2,
+      y: '0%',
+      duration: 0.7,
+      ease: 'power3.inOut'
+    })
+    TL1.to('.C2_img_Right', {
+      rotate: rightRotate,
+      duration: 0.7,
+      ease: 'power3.inOut'
+    }, 'a4')
+    TL1.to('.C2_img_left', {
+      rotate: leftRotate,
+      duration: 0.7,
+      ease: 'power3.inOut'
+    }, 'a4')
+    TL1.to('.cont-3', {
+      opacity:1,
+      duration: 0.7,
+      ease: 'power3.inOut'
+    }, 'a4')
+
+
   }
 
+  const slideShow = () => {
+    gsap.to('.slide-text-box',{
+      x:'-50%',
+      duration:10,
+      ease:'linear',
+      repeat: -1 
+    })
+  }
 
   // ---------------------------------------- 
   useEffect(() => {
@@ -106,6 +163,7 @@ const App = () => {
 
   useEffect(() => {
     welcomeAnimation()
+    slideShow()
   }, [])
 
 
@@ -158,21 +216,21 @@ const App = () => {
             <span className='TrapM text-[#efefef] text-[2rem] leading-[2rem] z-[90]'>CF</span>
 
             {/* Menu-Btn */}
-            <div className='w-fit h-fit p-[2px] border-[1px] border-[#efefef] rounded-full flex justify-center items-center select-none cursor-pointer z-[90]'>
+            <div className='w-fit h-fit p-[2px] border-[1px] border-[#efefef] rounded-full gap-[2px] flex justify-center items-center select-none cursor-pointer z-[90]'>
 
               {/* Circle */}
-              <div className='w-[40px] h-[40px] rounded-full flex justify-center items-center bg-[#efefef]'>
-                < FaArrowTurnUp className='text-[#410e0e] rotate-[180deg] text-[1.1rem]' />
+              <div className='w-[30px] h-[30px] rounded-full flex justify-center items-center bg-[#efefef]'>
+                < FaArrowTurnUp className='text-[#410e0e] rotate-[180deg] text-[0.8rem]' />
               </div>
 
               {/* Menu Div */}
               <div className='w-fit h-fit p-[2px] flex bg-[#efefef] rounded-full pl-[20px] justify-center items-center'>
 
-                <span className='TrapM text-[#272727] text-[1.2rem] leading-[1.2rem] pr-[10px]'>Menu</span>
+                <span className='TrapM text-[#272727] text-[1rem] leading-[1rem] pr-[10px] mt-[2px]'>Menu</span>
 
                 {/* Menu Circle */}
-                <div className='w-[40px] h-[40px] rounded-full bg-[#410e0e] flex justify-center items-center'>
-                  <HiOutlineMenu className='text-[#efefef] text-[1.5rem]' />
+                <div className='w-[30px] h-[30px] rounded-full bg-[#410e0e] flex justify-center items-center'>
+                  <HiOutlineMenu className='text-[#efefef] text-[1rem]' />
                 </div>
 
               </div>
@@ -181,41 +239,75 @@ const App = () => {
         </div>
 
         {/* Container-2 ~ Home Main */}
-        <div className='w-full h-fit bg-[#202020] flex flex-col  max-sm:px-[20px] sm:px-[40px] items-center justify-center overflow-hidden'>
+        <div className='w-full h-fit bg-[#202020] flex flex-col  max-sm:px-[20px] sm:px-[40px] items-center justify-center md:items-start overflow-hidden relative'>
 
-          {/* Title For ~ MOBILE */}
-          <div className=' md:hidden w-full h-fit flex flex-col max-md:mt-[40px] z-[80] justify-center items-center max-w-[370px] text-center'>
-            <h1 className='TrapB text-[2.4rem] leading-[2.4rem] text-[#efefef] tracking-tight'>Discover Style & Find Your Perfect Pair & Own Style.</h1>
-            <p className='px-[10px] mt-[17px] text-[1.2rem] leading-[1.2rem] text-[#D4D4D4]'>Unlock your true pontential proven strategies to sharpen  your mind, boost productivity and achive what matters most.</p>
+          {/* Title For All Devices  */}
+          <div className=' w-full h-fit flex flex-col max-md:mt-[40px] z-[80] will-change-transform justify-center items-center max-w-[370px] md:max-w-[600px] md:mt-[20px] lg:max-w-full  text-center md:text-start lg:items-start select-none'>
+            <h1 className='TrapB TTtitle text-[2.4rem] leading-[2.4rem] opacity-0  md:text-[2rem] md:leading-[2rem] text-[#efefef] tracking-tight lg:max-w-[500px] '>Discover Style & Find Your Perfect Pair & Own Style.</h1>
+            <div className='w-fit h-fit  flex lg:text-justify lg:w-full lg:justify-end'>
+              <p className='px-[10px] TT-p mt-[17px] md:mt-[10px] opacity-0 will-change-transform text-[1.2rem] leading-[1.2rem]  md:text-[1rem] md:leading-[1rem] md:px-[0px] text-[#D4D4D4] lg:max-w-[400px]'>Unlock your true pontential proven strategies to sharpen  your mind, boost productivity and achive what matters most.</p>
+            </div>
           </div>
 
-          {/* All Images Mid-Center */}
-          <div className='w-[300px] h-[470px] z-[99] max-sm:mt-[40px] mt-[90px] relative max-sm:w-[250px] max-sm:h-[370px]'>
 
-            {/* Main Image */}
-            <div className='w-full h-full bg-cyan-400 absolute top-0 z-[80] max-sm:w-[250px] overflow-hidden rounded-[10px] '>
-              <img className='w-full h-full object-cover object-center' src={img1} alt="" />
-            </div>
+          {/* 3_IMG */}
+          <div className='w-full h-fit flex flex-col justify-center items-center'>
 
-            {/* Main-Right-IMG */}
-            <div className='w-full h-full bg-orange-400 absolute top-6 origin-bottom rotate-20 max-md:rotate-12 max-sm:rotate-8 max-sm:w-[250px] overflow-hidden rounded-[10px] z-[79]'>
-              <img className='w-full h-full object-cover object-center' src={img2} alt="" />
-            </div>
+            {/* All Images Mid-Center */}
+            <div className='C2_img_main w-[300px] h-[470px] z-[99] max-sm:mt-[40px] mt-[90px] md:mt-[40px] relative max-sm:w-[250px] max-sm:h-[370px] translate-y-[130%]'>
 
-            {/* Main-Leftt-IMG */}
-            <div className='w-full h-full bg-pink-400 absolute top-6 origin-bottom -rotate-20 max-md:-rotate-12 max-sm:-rotate-8 max-sm:w-[250px] overflow-hidden rounded-[10px] z-[79]'>
-              <img className='w-full h-full object-cover object-center' src={img3} alt="" />
+              {/* Main Image */}
+              <div className=' w-full h-full bg-cyan-400 absolute top-0 z-[80] max-sm:w-[250px] overflow-hidden rounded-[10px] '>
+                <img className='w-full h-full object-cover object-center' src={img1} alt="" />
+              </div>
+
+              {/* Main-Right-IMG */}
+              <div className='C2_img_Right w-full h-full bg-orange-400 absolute top-6 origin-bottom rotate-0 max-md:rotate-0 max-sm:rotate-0 max-sm:w-[250px] overflow-hidden rounded-[10px] z-[79]'>
+                <img className='w-full h-full object-cover object-center' src={img2} alt="" />
+              </div>
+
+              {/* Main-Leftt-IMG */}
+              <div className='C2_img_left w-full h-full bg-pink-400 absolute top-6 origin-bottom rotate-0 max-md:-rotate-0 max-sm:-rotate-0 max-sm:w-[250px] overflow-hidden rounded-[10px] z-[79]'>
+                <img className='w-full h-full object-cover object-center' src={img3} alt="" />
+              </div>
+
             </div>
 
           </div>
+
 
         </div>
 
         {/* Container-3 ~ Logo-Slider */}
-        <div className='w-full h-[400px] bg-[#202020]'>
+        <div className='cont-3 opacity-0 w-full h-fit bg-[#202020] overflow-hidden flex pt-[50px] select-none'>
+          
+          <span className='slide-text-box w-fit flex text-[5rem] TrapSB text-[#efefef] whitespace-nowrap will-change-transform'>
+            <h1>CLEAR FOCUS</h1>
+            <h1 className='text-[#bb1212]'>*</h1>
+            <h1 className=' blur-[4px]'>CLEAR FOCUS</h1>
+            <h1 className='text-[#bb1212]'>*</h1>
+            <h1>CLEAR FOCUS</h1>
+            <h1 className='text-[#bb1212]'>*</h1>
+            <h1 className=' blur-[4px]'>CLEAR FOCUS</h1>
+            <h1 className='text-[#bb1212]'>*</h1>
+          </span>
+          <span className='slide-text-box w-fit flex text-[5rem] TrapSB text-[#efefef] whitespace-nowrap will-change-transform'>
+            <h1>CLEAR FOCUS</h1>
+            <h1 className='text-[#bb1212]'>*</h1>
+            <h1 className=' blur-[4px]'>CLEAR FOCUS</h1>
+            <h1 className='text-[#bb1212]'>*</h1>
+            <h1>CLEAR FOCUS</h1>
+            <h1 className='text-[#bb1212]'>*</h1>
+            <h1 className=' blur-[4px]'>CLEAR FOCUS</h1>
+            <h1 className='text-[#bb1212]'>*</h1>
+          </span>
 
         </div>
 
+        {/* Container-4 */}
+        <div className='w-full h-screen flex bg-[#202020]'>
+
+        </div>
 
 
       </div>
